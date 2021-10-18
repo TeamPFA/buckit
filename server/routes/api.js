46834eac37d9d104c2c9a529f6e4f4ea4e7ee0c6
Ron Fu <ronfu85@gmail.com>
2021-10-17 18:09:57 -0700

    new db schema, GET/POST Co-authored: Sean Lee Co-authored: William Cheng

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
+
 const apiController = require('../controller/apiController.js')
 
-//send username, password to sql db
-router.post('/', apiController.createUser)
+router.get('/', (req, res, next) => res.status(200).send('/api GET REQUEST SUCCESSFUL'));
 
-module.exports = router;  
+router.post('/signup', apiController.createUser);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,24 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
-const apiRouter = require('./routes/api.js');
+
 const PORT = process.env.PORT || 3000;
-const apiController = require('./controller/apiController.js')
+
 // Required middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-// Routes directory for all routing
-// const testRouter = require('./routes/api)
-// app.use('/api', testRouter);
-
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
-})
-
 // Routes to api router 
+const apiRouter = require('./routes/api.js');
 app.use('/api', apiRouter); 
 
 // Local error handler
-app.use('*', (req, res) => res.status(404).send('404: PAGE DOES NOT EXIST'));
+app.use((req, res) => res.status(404).send('404: PAGE DOES NOT EXIST'));
 
 // Global error handler
-app.use((err, req, res, next) => {
-  const defaultError = {
-    log: 'Error handler caught middleware error'
-  }
-
-  return err;
-})
+app.use((err, req, res, next) => res.status(500).json(err));
 
 app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));;
 
